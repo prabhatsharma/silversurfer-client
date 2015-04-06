ecec373c2168edf91e235cf263576dc1db02609f
Prabhat <hi.prabhat@gmail.com>
2015-04-06 15:41:02 +0530

    add gulp support

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var browserify = require('browserify');
 var transform = require('vinyl-transform');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var minifyHTML = require('gulp-minify-html');
 
 //jshint task
 gulp.task('jshint', function() {
@@ -13,13 +14,30 @@ gulp.task('jshint', function() {
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('browserify', function () {
+gulp.task('minify-js', function () {
+
+  // use `vinyl-transform` to wrap the regular ReadableStream returned by `b.bundle();` with vinyl file object
+  // so that we can use it down a vinyl pipeline
+  // while taking care of both streaming and buffered vinyl file objects
   var browserified = transform(function(filename) {
-    var b = browserify(filename);
-    return b.bundle();
+    // filename = './app.js' in this case
+    return browserify(filename)
+      .bundle();
   });
-  return gulp.src(['app.js', './scripts/**/*.js'])
+
+  return gulp.src(['./app.js']) // you can also use glob patterns here to browserify->uglify multiple files
     .pipe(browserified)
     .pipe(uglify())
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('minify-html', function() {
+  var opts = {
+    conditionals: true,
+    spare:true
+  };
+ 
+  return gulp.src('./static/html/*.html')
+    .pipe(minifyHTML(opts))
+    .pipe(gulp.dest('./dist/'));
 });

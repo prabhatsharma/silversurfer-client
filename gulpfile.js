80939ef22a95475d9c1cddba4bde43ca6bfbc33f
Prabhat Sharma <hi.prabhat@gmail.com>
2015-04-07 11:13:19 +0530

    add gulp tasks and reorganize folders

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,20 @@ var browserify = require('browserify');
 var transform = require('vinyl-transform');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var gutil = require('gulp-util');
 var minifyHTML = require('gulp-minify-html');
+var minifyCSS = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var connect = require('gulp-connect');
+	
+gulp.task('serve', ['watch'], function() {
+	connect.server({
+		root: 'dist',
+		livereload: true
+	  });
+});
 
 //jshint task
 gulp.task('jshint', function() {
@@ -25,19 +38,52 @@ gulp.task('minify-js', function () {
       .bundle();
   });
 
-  return gulp.src(['./app.js']) // you can also use glob patterns here to browserify->uglify multiple files
+  return gulp.src(['./src/app.js']) // you can also use glob patterns here to browserify->uglify multiple files
     .pipe(browserified)
     .pipe(uglify())
     .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('minify-html', function() {
-  var opts = {
-    conditionals: true,
-    spare:true
-  };
- 
-  return gulp.src('./static/html/*.html')
-    .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./dist/'));
+gulp.task('move-index', function(){
+	return gulp.src(['./src/index.html'])
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('move-views', function(){
+	return gulp.src(['./src/views/*.html'])
+		.pipe(gulp.dest('./dist/views'));
+});
+
+gulp.task('move-vendorcss', function() {
+  return gulp.src(['./bower_components/bootstrap/dist/css/bootstrap.min.css'])
+    .pipe(gulp.dest('./dist/styles/vendor'));
+});
+
+gulp.task('minify-mycss', function() {
+
+  return gulp.src(['./src/styles/*.css'])
+  	.pipe(concat('./tmp/style.css'))
+  	.pipe(minifyCSS({keepBreaks:true}))
+  	.pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('move-images', function(){
+	return gulp.src(['./src/images/*.*'])
+		.pipe(gulp.dest('./dist/images'));
+});
+
+gulp.task('move-html', ['move-index', 'move-views']);
+gulp.task('minify-css', ['minify-mycss', 'move-vendorcss']);
+gulp.task('build', ['minify-js', 'minify-css', 'move-html', 'move-images']);
+
+gulp.task('watch',['build'], function(){
+	
+	var htmlfiles = ['./src/index.html','./src/views/*.html'];
+	var cssfiles = ['./bower_components/bootstrap/dist/bootstrap.min.css','./src/styles/*.css'];
+	var jsfiles = ['app.js', './src/scripts/controllers/*.js', './src/scripts/services/*.js'];
+	
+	gulp.watch(htmlfiles, ['move-html']);
+	gulp.watch(cssfiles, ['minify-css']);
+	gulp.watch(jsfiles, ['minify-js']);
 });
--- src/scripts/controllers/addboardController.js
+++ src/scripts/controllers/addboardController.js
@@ -8,9 +8,7 @@ module.exports = ['$scope', '$location', 'boardService', function($scope, $locat
 		console.log('inside addboardController.saveData()');
         boardService.saveData(newBoard).$promise.then(function() {
             console.log('Successfully inserted ' + newBoard.location);
-			//$state.state = '/';
 			$location.path('/');
-            //$scope.projects.splice(1,0,newProject);
         });        
         $scope.newProject = {};
     };
